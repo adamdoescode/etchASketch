bfa50bcd2497d3776eede5ffbf7360e780947148
Adamdoescode <13943324+adamdoescode@users.noreply.github.com>
2023-02-09 14:44:00 +0800

    make working hover effect

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ function makeGrid() {
             // make grid
             newDiv = document.createElement('div')
             newDiv.classList.add('drawDiv')
+            // add a unique class name for hover effect
+            newDiv.classList.add(`${row}-${col}`)
             // add some styling to make them visible
             // newDiv.style.cssText = 'background-color: blue;'
+            newDiv.addEventListener('mouseover', (event) => {
+                event.target.style.backgroundColor = 'purple';
+            })
+            // and when it leaves
+            newDiv.addEventListener('mouseleave', (event) => {
+                setTimeout(() => {event.target.style.backgroundColor = 'aquamarine'}, 10)
+            })
+            // add to parent
             newRow.appendChild(newDiv)
         }
         //add row div to drawingBoardDiv
@@ -21,4 +31,10 @@ function makeGrid() {
     }
 }
 
+// run makeGrid at startup
 makeGrid()
+
+drawingBoardDiv.addEventListener('mouseenter', (event) => {
+    event.target.style.color = 'purple';
+    setTimeout(() => {event.target.style.color = 'aquamarine'}, 500)
+})
